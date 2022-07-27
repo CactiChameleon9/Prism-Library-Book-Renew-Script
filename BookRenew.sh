6b4568fc84968c702c1265ad325b1183d646e0a5
CactiChameleon9 <51231053+CactiChameleon9@users.noreply.github.com>
2022-07-27 07:49:31 +0000

    Improve the scripts a lot!
    Added sleep, echo and error stuff to prevent loads of issues with bash.

--- BookRenew.sh
+++ BookRenew.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+set -euo pipefail
+
 LIB_CARD=
 LIB_PIN=
 
 PRISM_URL=https://
 
-# get cookies/session
+echo 'Logging in to get cookies/session'
 curl $PRISM_URL/sessions -X POST -d "barcode=$LIB_CARD&institutionId=&pin=$LIB_PIN&borrowerLoginButton=Login" -c myLibraryCookies
 
-# get books to renew
+sleep 1
+
+echo 'Getting the list of books to renew'
 BOOK_IDS=$(curl $PRISM_URL/account/loans -b myLibraryCookies -silent | grep loan_ids | sed -n '/.*/{ s/value="/%%%/;s/^.*%%%//; s/".*//; p; }' | sort -u)
 
-# put in the post data format
+sleep 1
+
+echo 'Formatting that data into the post data format'
 RENEW_DATA=$(echo $BOOK_IDS | sed -n 's/ /\&loan_ids%5B%5D=/g;p' | sed -n 's/^/loan_ids%5B%5D=/;p')
 
-# get renew books
+sleep 1
+
+echo 'Renewing the books'
 curl $PRISM_URL/account/loans -X POST -d "$RENEW_DATA" -b myLibraryCookies -silent
+
+sleep 1
+echo Done!!
